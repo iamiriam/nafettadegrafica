138c4cf9674ab12a4e42c60253f213ae88c2b2e9
Miriam <miriam.reho93@gmail.com>
2023-04-03 12:02:48 +0200

    setup layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+function Layout (){
+
+    return (
+        <>
+            <p>Sono in Layout</p>
+            {/* inserisci qui i <nomecomponente/> */}
+        </>
+    );
+}
+
+export default Layout
