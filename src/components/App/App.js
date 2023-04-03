63cc692d3ec5018ca0eb5fdbe997e6e70e9b045e
Miriam <miriam.reho93@gmail.com>
2023-04-03 18:26:38 +0200

    aggiunto animejs test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Layout from '../Layout';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
+import Player from "../Player";
+
 function Layout (){
 
     return (
         <>
             <p>Sono in Layout</p>
             {/* inserisci qui i <nomecomponente/> */}
+            <Player/>
         </>
     );
 }
 
-export default Layout
+export default Layout;
